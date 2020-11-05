f67170e122b0320e5cf374b9e4a7278eb941fdae
Pablo Sanchez <pablo.sanchez@flagbit.de>
2020-11-06 00:21:09 +0100

    Get referenced document

--- src/app/shared/services/training.service.ts
+++ src/app/shared/services/training.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { IExercise } from '../models/exercise.interface';
 import { ITraining } from '../models/training.interface';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -21,17 +21,40 @@ export class TrainingService {
     return this.getTrainingById('default');
   }
 
-  public getTrainingById(id: string): Observable<void> { // <ITraining> {
+  public getTrainingById(id: string): Observable<ITraining> {
+    const exercise: IExercise = {
+      id: 'test',
+      description: 'bla',
+    };
+    const trainingDemo: ITraining = {exercises: [exercise]};
+
     return this.trainingsCollection.doc(id).valueChanges().pipe(
-      map((actions) => {
-        actions.map((a) => {
-          const data = a.payload.doc.data() as ITraining;
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        }),
-      })
+      map((training) => {
+        console.log(training);
+      }),
+      map(() => trainingDemo)
     );
   }
+
+  /* how to get referenced document
+  this.feedCollection = this.afs.collection('col-challange');
+  this.feedItem = this.feedCollection.snapshotChanges().map(changes => {
+      return changes.map(a => {
+        //here you get the data without first name
+        const data = a.payload.doc.data() as Feed;
+        //get the signup_id for getting doc from coll-signup
+        const signupId = data.signup_id;
+        //get the related document
+        return afs.collection('coll-signup').doc(signupId).snapshotChanges().take(1).map(actions => {
+          return actions.payload.data();
+        }).map(signup => {
+          //export the data in feeds interface format
+          return { firstName: signup.firstName, ...data };
+        });
+      })
+    }).flatMap(feeds => Observable.combineLatest(feeds));
+    https://stackoverflow.com/questions/47174075/firestore-how-to-get-the-collection-value-from-another-collection-document-id-is
+  */
 
   public getExerciseById(id: string): Observable<IExercise> {
     return this.exercisesCollection.doc<IExercise>(id).valueChanges();
