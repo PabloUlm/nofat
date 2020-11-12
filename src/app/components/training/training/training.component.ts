b2a0ab55f1fbfcca95f6298437bb82a7c0571a58
Pablo Sanchez <pablo.sanchez@flagbit.de>
2020-11-13 00:12:17 +0100

    timer - first approach

--- src/app/components/training/training/training.component.ts
+++ src/app/components/training/training/training.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { interval, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { ITraining } from '../../../shared/models/training.interface';
 import { TrainingService } from '../../../shared/services/training.service';
 
@@ -10,12 +11,37 @@ import { TrainingService } from '../../../shared/services/training.service';
 })
 export class TrainingComponent implements OnInit {
   public isWorkingOut = false;
+  public timer: Observable<string>;
+  public defaultExerciseTime = 600; // 10 minutes
+
   constructor(public trainingSvc: TrainingService) { }
 
   ngOnInit(): void {
     this.trainingSvc.defaultTraining$.subscribe((result) => {
       console.log('RESULT:::::', result);
     });
+  }
+
+  public transformTime(seconds: number): string {
+    const minutes: number = Math.floor(seconds / 60);
+    return minutes + ':' + (seconds - minutes * 60);
+  }
+
+  public exerciseFinished(): void {
+    console.log('finished');
+  }
+
+  public startExercise(): void {
+    // this.noSleep.enable();
+    this.isWorkingOut = true;
+    this.timer = interval(1000).pipe(
+      map(x => this.transformTime(this.defaultExerciseTime - x)),
+      tap(time => {
+        if (time === '0:00') {
+          this.exerciseFinished();
+        }
+      })
+    );
   }
 
 }
